8cdf3d2540c443f4ea53cf31d79b4feb5b64e17d
Glen Rose <glenrose83@gmail.com>
2023-09-13 16:46:23 +0200

    new file:   bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'includes/db_connection.php';
+include_once 'functions/cart_functions.php';
+include_once 'admin/functions/admin_functions.php';
+include_once 'admin/functions/edit_product_functions.php';
+
+//classes that must be used in autoloader
+include_once 'admin/classes/db.class.php';
+include_once 'admin/classes/handle_order.class.php';
+include_once 'admin/classes/settings.class.php';
+
+
+
